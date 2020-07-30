8690f3762cf891ec44ea758b4929e8075d7b8765
Priest <marioskpappas@gmail.com>
2020-07-30 23:25:47 +0300

    -

---
+++ angular/Pharmaceutical-Clinical-Trials/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FlightsComponent } from './flights/flights.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FlightsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
