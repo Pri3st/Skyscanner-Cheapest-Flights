b7f81a9c85da4602c321331cffdcf549f107ab7d
Priest <marioskpappas@gmail.com>
2020-08-22 15:08:57 +0300

    22/08/2020

--- angular/Pharmaceutical-Clinical-Trials/src/app/app.module.ts
+++ angular/Pharmaceutical-Clinical-Trials/src/app/app.module.ts
@@ -4,23 +4,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlightsComponent } from './flights/flights.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NavigationBarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import {Routes} from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import {ReactiveFormsModule, Validators} from '@angular/forms';
+
+
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'search', component: FlightsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FlightsComponent,
-    NavigationBarComponent,
+    HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-  ],
+    ReactiveFormsModule
+      ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/Pharmaceutical-Clinical-Trials/src/app/search/search.component.spec.ts
+++ angular/Pharmaceutical-Clinical-Trials/src/app/search/search.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationBarComponent } from './navbar.component';
+import { SearchComponent } from './search.component';
 
-describe('NavbarComponent', () => {
-  let component: NavigationBarComponent;
-  let fixture: ComponentFixture<NavigationBarComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationBarComponent ]
+      declarations: [ SearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationBarComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
